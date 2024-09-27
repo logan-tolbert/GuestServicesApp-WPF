c304e8d8499e05ccae0a5a638a3f307e9610085e
logan-tolbert <logo88205+git@gmail.com>
2024-09-27 17:33:53 -0500

    :tada: Initial commit

---
+++ DataAccessLibrary/DataAccess/MySqlDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestServicesLibrary.DataAccess
+{
+    public static class  MySqlDataAccess
+    {
+        public static string GetServerConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+            string? connString = config.GetConnectionString("DefaultConnection");
+            return connString;
+        }
+       
+    }
+}
---
+++ DataAccessLibrary/Models/GuestBookModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestServicesLibrary.Models
+{
+    internal class GuestBookModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public int GroupSize { get; set; }
+
+        public string? Comment { get; set; }
+       
+        //TODO: create properties to store date and time of visit
+        //private string _date { get; set;}
+        //private string _time { get; set; }
+
+    }
+}
